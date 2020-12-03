f615211d44e2f8daa88727301366f0446821eeb4
kentomiya89 <miyaba1289@gmail.com>
2020-12-03 15:23:19 +0900

    feat:位置情報設定の許諾(使用許可と正確性)の処理を追加

--- BikeSpotSearch/TopMapViewController.swift
+++ BikeSpotSearch/TopMapViewController.swift
@@ -13,6 +13,7 @@ class TopMapViewController: UIViewController {
     var locationManager: CLLocationManager! {
         didSet {
             locationManager.delegate = self
+            locationManager.distanceFilter = 10
         }
     }
 
@@ -28,17 +29,22 @@ class TopMapViewController: UIViewController {
         super.viewDidLoad()
 
         locationManager = CLLocationManager()
-        requestLocation()
+
+        if #available(iOS 14, *) {
+            // 何か後で記載するかも
+        } else {
+            // 従来のリクエスト
+            requestLocation()
+        }
     }
 
+    // iOS 13でのリクエスト
     func requestLocation() {
-        locationManager.requestAlwaysAuthorization()
-        let status = CLLocationManager.authorizationStatus()
-
-        switch status {
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
         case .authorizedWhenInUse,
              .authorizedAlways:
-            locationManager.distanceFilter = 10
             locationManager.startUpdatingLocation()
         default:
             break
@@ -49,6 +55,33 @@ class TopMapViewController: UIViewController {
 
 // MARK: CLLocationManagerDelegate
 extension TopMapViewController: CLLocationManagerDelegate {
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // 位置情報許可
+        if #available(iOS 14.0, *) {
+            switch manager.authorizationStatus {
+            case .notDetermined:
+                // 位置情報
+                manager.requestWhenInUseAuthorization()
+            case .authorizedAlways, .authorizedWhenInUse:
+                locationManager.startUpdatingLocation()
+            default:
+                break
+            }
+        }
+
+        // 正確性
+        if #available(iOS 14.0, *) {
+            let accuracyAuthorization = manager.accuracyAuthorization
+            switch accuracyAuthorization {
+            case .reducedAccuracy:
+                manager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "Accuracy")
+            default:
+                break
+            }
+        }
+
+    }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         for location in locations {
