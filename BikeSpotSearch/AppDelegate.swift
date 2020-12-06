7fd3569889bb918188105e6b3fa5976511dbae09
kentomiya89 <miyaba1289@gmail.com>
2020-12-06 15:17:19 +0900

    delete:SceneDelegateを削除

--- BikeSpotSearch/AppDelegate.swift
+++ BikeSpotSearch/AppDelegate.swift
@@ -11,22 +11,14 @@ import GoogleMaps
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         if let apikey = getGoogleMapKey {
             GMSServices.provideAPIKey(apikey)
         }
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 
 }
