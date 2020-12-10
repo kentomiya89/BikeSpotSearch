0141eddd69e3177d57de7e6eaf5f17e23942e074
kentomiya89 <miyaba1289@gmail.com>
2020-12-11 04:09:24 +0900

    feat:データの追加削除のテストコードを追加

--- BikeSpotSearchTests/BikeSpotSearchTests.swift
+++ BikeSpotSearchTests/BikeSpotSearchTests.swift
@@ -9,6 +9,20 @@ import XCTest
 @testable import BikeSpotSearch
 
 class BikeSpotSearchTests: XCTestCase {
+    
+    let accessor = MyBikeParkAccessor()
+
+    override func setUp() {
+        super.setUp()
+        clearMyBikePark()
+    }
+
+    func clearMyBikePark() {
+        let myBikeParkArray = accessor.fetchAll()
+        myBikeParkArray.map {
+            try! accessor.delete($0)
+        }
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,10 +32,25 @@ class BikeSpotSearchTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRealmAddAndDeleteTest() throws {
+        try XCTContext.runActivity(named: "データの保存削除のテスト") { _ in
+            let shinjyuku = MyBikePark().shinjyuku
+            try accessor.add(shinjyuku)
+            // 1個
+            XCTAssertEqual(accessor.myBikeParkCount(), 1)
+            
+            let shibuya = MyBikePark().shibuya
+            try accessor.add(shibuya)
+            // 2個
+            XCTAssertEqual(accessor.myBikeParkCount(), 2)
+            
+            try accessor.delete(shinjyuku)
+            try accessor.delete(shibuya)
+            // 0個
+            XCTAssertEqual(accessor.myBikeParkCount(), 0)
+        }
     }
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
